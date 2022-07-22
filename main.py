3a19d7a92d6b784340c92715f6c34f63988a8707
Pavlovskii21 <109367073+Pavlovskii21@users.noreply.github.com>
2022-07-22 20:25:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import math
- a*x^2 +bx + c = 0
+# a*x^2 +bx + c = 0
+a = float(input('Ввведите коэффициент а:'))
+b = float(input('Ввведите коэффициент b:'))
+c = float(input('Ввведите коэффициент c:'))
 
-a = int(input())
-b = int(input())
-c = int(input())
-
-D = b ** 2 - 4*a*c
-Droot = math.sqrt(D)
-# x = (-b -+ sqrt(D))/(2a)
-#если D > 0
-#x = -b/2a
-#если D == 0
-#нет корней
-#если D < 0
-if D > 0
- x=(-b-+sqrt(D))/(2a)
-elif D == 0
- x = -b/2a
-else D < 0
- x=0
+D = b ** 2 - 4 * a * c
+if D > 0:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+    print(x1, x2)
+elif D == 0:
+    x = -b / (2 * a)
+    print(x)
+else:
+    print('Нет корней')
